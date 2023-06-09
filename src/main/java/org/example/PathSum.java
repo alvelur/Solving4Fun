8fc4b852b6932f32cd71e505d2dfebf6298bf752
Alejandro Velasquez <alejandrovelasquezuribe@gmail.com>
2023-06-09 18:01:43 -0500

    Solving Fourth Programming challenge

--- src/main/java/org/example/PathSum.java
+++ src/main/java/org/example/PathSum.java
@@ -11,15 +11,10 @@ public class PathSum {
 
     public boolean hasPathSum(TreeNode root, int targetSum) {
 
-        if (root == null && targetSum == 0) return true;
-        else {
-            if (root == null) return false;
+        if (root == null) return false;
 
-        }
         Queue<TreeNode> treeQueue = new LinkedList<>();
         Queue<Integer> count = new LinkedList<>();
-
-
 
         treeQueue.add(root);
         count.add(root.val);
@@ -27,17 +22,16 @@ public class PathSum {
         while(!treeQueue.isEmpty()){
             TreeNode actual = treeQueue.remove();
             int sum = count.remove();
+            System.out.println(sum);
 
-            if(actual.left != null || actual.right != null) {
                 if(actual.left != null ){
                     treeQueue.add(actual.left);
-                    count.add(actual.left.val + actual.val);
+                    count.add(actual.left.val + sum);
                 }
                 if (actual.right != null) {
                     treeQueue.add(actual.right);
-                    count.add(actual.right.val  + actual.val);
+                    count.add(actual.right.val  + sum);
                 }
-            }
 
             if(actual.right == null && actual.left ==null){
                 if(targetSum == sum){
