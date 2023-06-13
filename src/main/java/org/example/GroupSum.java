269dfcc3d2a72366be5e5863fc25db21f9641c6f
Alejandro Velasquez <alejandrovelasquezuribe@gmail.com>
2023-06-12 22:14:57 -0500

    Solving 5 day doing Programming challenge

---
+++ src/main/java/org/example/GroupSum.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class GroupSum {
+    public static void main(String[] args) {
+
+    }
+
+    //https://codingbat.com/prob/p145416
+    public boolean groupSum(int start, int[] nums, int target) {
+        if(start >= nums.length){
+            return target == 0;
+        }
+        if(groupSum(start +1, nums, target - nums[start])) return true;
+        if(groupSum(start +1, nums, target)) return true;
+
+        return false;
+    }
+}
