3a392cb9971a377f0a85bd1c78f09b3228e18135
Alejandro Velasquez <alejandrovelasquezuribe@gmail.com>
2023-06-12 21:19:12 -0500

    Solving 5 day doing Programming challenge

---
+++ src/main/java/org/example/RecursiveFactorial.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class RecursiveFactorial {
+
+    //Factorial
+    public int factorial(int n) {
+        if(n > 0){
+            return n * factorial(n-1);
+        } else
+            return 1;
+    }
+}
