1e72b8b7f8345286966ba3482fd29a2db6159763
Alejandro Velasquez <alejandrovelasquezuribe@gmail.com>
2023-06-13 23:15:59 -0500

    Today I will start with some API testing

---
+++ src/test/java/org/example/endpoints/Routes.java
@@ -0,0 +1,17 @@
+package org.example.endpoints;
+
+
+public class Routes {
+
+    /* Publica para usarla en todo el proyecto y estatica para poder usarla creando un objeto rutas
+     */
+    public static String baseUrl = "https://petstore.swagger.io/v2";
+
+    //Modelo del usuario
+
+    public static String post_User =  baseUrl + "/user";
+    public static String get_User = baseUrl + "/user/{username}";
+    public static String put_User = baseUrl + "/user/{username}";
+    public static String delete_User = baseUrl + "/user/{username}";
+
+}
---
+++ src/test/java/org/example/endpoints/UserEndPoints.java
@@ -0,0 +1,53 @@
+package org.example.endpoints;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class UserEndPoints {
+
+    public static Response createAnUser(User payload){
+        Response response =
+                given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(payload)
+                .when()
+                //Como es estatico podemos llamarlo directamente con la clase
+                .post(Routes.post_User);
+        return response;
+    }
+
+    public static Response getAnUser(String userName){
+        Response response =
+                given()
+                        .pathParam("username", userName)
+                        .when()
+                        .get(Routes.get_User);
+        return response;
+    }
+
+    public static Response updateAnUser(User payload, String userName){
+        Response response =
+                given()
+                        .contentType(ContentType.JSON)
+                        .accept(ContentType.JSON)
+                        .pathParam("username", userName)
+                        .body(payload)
+                        .when()
+                        //Como es estatico podemos llamarlo directamente con la clase
+                        .put(Routes.put_User);
+        return response;
+    }
+
+    public static Response deleteAnUser(String userName){
+        Response response =
+                given()
+                        .pathParam("username", userName)
+                        .when()
+                        .delete(Routes.delete_User);
+        return response;
+    }
+
+}
